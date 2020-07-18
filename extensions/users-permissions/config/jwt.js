8ff63c42cba95e3d92c237fbcdf9d212ba077148
PireJoseph <he201388@students.ephec.be>
2020-07-18 15:44:44 +0200

    first commit 🚀

---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6e79a913-5f65-489c-a04d-116aa6261e36'
+};
---
+++ extensions/users-permissions/controllers/User.js
@@ -0,0 +1,114 @@
+'use strict';
+
+/**
+ * User.js controller
+ *
+ * @description: A set of functions called "actions" for managing `User`.
+ */
+
+const _ = require('lodash');
+const { sanitizeEntity } = require('strapi-utils');
+
+const sanitizeUser = user =>
+  sanitizeEntity(user, {
+    model: strapi.query('user', 'users-permissions').model,
+  });
+
+const formatError = error => [
+  { messages: [{ id: error.id, message: error.message, field: error.field }] },
+];
+
+module.exports = {
+  /**
+   * Update a/an user record.
+   * @return {Object}
+   */
+  async update(ctx) {
+    const advancedConfigs = await strapi
+      .store({
+        environment: "",
+        type: "plugin",
+        name: "users-permissions",
+        key: "advanced",
+      })
+      .get();
+
+    const paramId = ctx.params.id;  
+    const user = ctx.state.user;
+    if (!user) {
+        return ctx.badRequest(null, [{ messages: [{ id: 'No authorization header was found' }] }]);
+    }
+    const id = user.id;
+    if(parseInt(paramId) !== id) {
+        return ctx.unauthorized(null, [{messages: [{id: 'You are not allowed to update another user profile'}]}]);
+    }
+
+    const { email, username, password } = ctx.request.body;
+
+
+    if (_.has(ctx.request.body, "email") && !email) {
+      return ctx.badRequest("email.notNull");
+    }
+
+    if (_.has(ctx.request.body, "username") && !username) {
+      return ctx.badRequest("username.notNull");
+    }
+
+    if (
+      _.has(ctx.request.body, "password") &&
+      !password &&
+      user.provider === "local"
+    ) {
+      return ctx.badRequest("password.notNull");
+    }
+
+    if (_.has(ctx.request.body, "username")) {
+      const userWithSameUsername = await strapi
+        .query("user", "users-permissions")
+        .findOne({ username });
+
+      if (userWithSameUsername && userWithSameUsername.id != id) {
+        return ctx.badRequest(
+          null,
+          formatError({
+            id: "Auth.form.error.username.taken",
+            message: "username.alreadyTaken.",
+            field: ["username"],
+          })
+        );
+      }
+    }
+
+    if (_.has(ctx.request.body, "email") && advancedConfigs.unique_email) {
+      const userWithSameEmail = await strapi
+        .query("user", "users-permissions")
+        .findOne({ email });
+
+      if (userWithSameEmail && userWithSameEmail.id != id) {
+        return ctx.badRequest(
+          null,
+          formatError({
+            id: "Auth.form.error.email.taken",
+            message: "Email already taken",
+            field: ["email"],
+          })
+        );
+      }
+    }
+
+    let updateData = {
+      ...ctx.request.body,
+    };
+
+    if (_.has(ctx.request.body, "password") && password === user.password) {
+      delete updateData.password;
+    }
+
+    const data = await strapi.plugins["users-permissions"].services.user.edit(
+      { id },
+      updateData
+    );
+
+    ctx.send(sanitizeUser(data));
+  },
+};
