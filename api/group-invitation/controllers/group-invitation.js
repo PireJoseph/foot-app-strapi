88d3052889a64534216cdce9a95a7e0a67dcba7b
JosephPire <he201388@students.ephec.be>
2020-08-05 17:45:07 +0200

    feat(API):  Add create group-invitation logic 👋

--- api/group-invitation/controllers/group-invitation.js
+++ api/group-invitation/controllers/group-invitation.js
@@ -8,6 +8,96 @@ const { sanitizeEntity } = require("strapi-utils");
  */
 module.exports = {
   /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+  async create(ctx) {
+    // get current user, the group invitation source
+    const user = ctx.state.user;
+    if (!user) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No authorization header was found" }] },
+      ]);
+    }
+
+    // get the group
+    const groupId = ctx.request.body.group;
+    if (!groupId) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No group id found" }] },
+      ]);
+    }
+    const group = await strapi.query("group").findOne({ id: groupId });
+    if (!group) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "There is no group with the id" + groupId }] },
+      ]);
+    }
+
+    // get the message
+    const message = ctx.request.body.message;
+
+    // get the recipients
+    const recipientId = ctx.request.body.recipient;
+    if (!recipientId) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No recipient found" }] },
+      ]);
+    }
+    const recipient = await strapi
+      .query("user", "users-permissions")
+      .findOne({ id: recipientId });
+    if (!recipient) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "There is no user with the id" + recipientId }] },
+      ]);
+    }
+    const alreadyExistingInvitationData = {
+      source: user.id,
+      recipient: recipientId,
+      group: groupId,
+    };
+    // check if an invitation has already been sent for this recipient
+    const alreadyExistingGroupInvitation = await strapi.services[
+      "group-invitation"
+    ].findOne(alreadyExistingInvitationData);
+    if (alreadyExistingGroupInvitation) {
+      const recipient = await strapi
+        .query("user", "users-permissions")
+        .findOne({ id: recipientId });
+      return ctx.badRequest(null, [
+        {
+          messages: [
+            {
+              id:
+                "There is already a group invitation for this group to user" +
+                recipient.username,
+            },
+          ],
+        },
+      ]);
+    }
+
+    const data = {
+      source: user.id,
+      recipient: recipientId,
+      group: groupId,
+    };
+    if (message) {
+      data["message"] = message;
+    }
+
+    // if there is not invitation for this source and this recipitent for this group, create one
+    const groupInvitation = await strapi.services["group-invitation"].create(
+      data
+    );
+
+    return sanitizeEntity(groupInvitation, {
+      model: strapi.models["group-invitation"],
+    });
+  },
+  /**
    * retrieve invitation made by the user
    *
    */
@@ -134,17 +224,17 @@ module.exports = {
 
     // update group invitation
     const groupInvitationUpdated = await strapi.services[
-        "group-invitation"
-      ].update(
-        { id: groupInvitationId },
-        {
-          isAccepted: false,
-          isAnswered: true,
-        }
-      );
+      "group-invitation"
+    ].update(
+      { id: groupInvitationId },
+      {
+        isAccepted: false,
+        isAnswered: true,
+      }
+    );
 
-      return sanitizeEntity(groupInvitationUpdated, {
-        model: strapi.models["group-invitation"],
-      });
+    return sanitizeEntity(groupInvitationUpdated, {
+      model: strapi.models["group-invitation"],
+    });
   },
 };
