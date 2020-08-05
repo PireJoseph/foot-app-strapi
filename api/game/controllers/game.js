af2adaf01f6db32df8880d824613c020eecca010
JosephPire <he201388@students.ephec.be>
2020-08-05 17:42:38 +0200

    feat(API): implement custom game endpoints ⚽

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,49 @@
-'use strict';
+"use strict";
+
+// Public dependencies.
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  // retrieve my next games
+  async myNext(ctx) {
+    const user = ctx.state.user;
+    if (!user) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No authorization header was found" }] },
+      ]);
+    }
+
+    const data = await strapi.services.game.findMyNext(user.id);
+    return data.map((game) =>
+      sanitizeEntity(game, { model: strapi.models.game })
+    );
+  },
+
+  // join a team in a game
+  async joinTeam(ctx) {
+    const user = ctx.state.user;
+    if (!user) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No authorization header was found" }] },
+      ]);
+    }
+
+    const gameId = ctx.params.id;
+
+    const { team } = ctx.request.body;
+    if (!team || (!team === 'a' && !team === 'b')) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "no valid team identification found" }] },
+      ]);
+    }
+
+    const data = await strapi.services.game.joinTeam(gameId, user, team);
+
+    return sanitizeEntity(data, { model: strapi.models.game });
+  },
+};
--- api/game/services/game.js
+++ api/game/services/game.js
@@ -1,8 +1,49 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  findMyNext(userId) {
+    const now = new Date().toISOString();
+    const params = {
+      players: [userId],
+      startAt_gt: now,
+    };
+
+    return strapi
+      .query("game")
+      .find(params, ["players", "teamAMembers", "teamBMembers"]);
+  },
+  async joinTeam(gameId, user, team) {
+    const params = {
+      id: gameId,
+    };
+    const game = await strapi
+      .query("game")
+      .findOne(params, ["players", "teamAMembers", "teamBMembers"]);
+    // filter out user from any group
+    let teamAMembers = game.teamAMembers.filter(
+      (member) => member.id !== user.id
+    );
+    let teamBMembers = game.teamBMembers.filter(
+      (member) => member.id !== user.id
+    );
+    // assign user to the correct group
+    if (team === "a") {
+      teamAMembers = [...teamAMembers, user];
+    } else if (team === "b") {
+      teamBMembers = [...teamBMembers, user];
+    }
+
+    const updatedGame = await strapi
+      .query("game")
+      .update(
+        { id: game.id },
+        { teamAMembers: teamAMembers, teamBMembers: teamBMembers }
+      );
+    return updatedGame;
+  },
+};
